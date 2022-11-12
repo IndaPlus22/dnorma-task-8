adc61432590cc4439c8592fe03b3dff372e812c0
Daniel Norman <dnorma@kth.se>
2022-11-12 11:21:54 +0100

    Clean up and change to correct registers

--- high-to-low/multiplication.asm
+++ high-to-low/multiplication.asm
@@ -1,59 +1,90 @@
 .data
+choice_msg: .asciiz "Choose either multiply or factorial. Type [1] or [0]"
+inv_input: .asciiz "Invalid input. Bye"
+multi_msg: .asciiz "Multiplying 2 integers"
+fact_msg: .asciiz "Factorializing(?) an integer"
 .text
+.spac
+main:
+	li $v0, 4
+	la $a0, choice_msg
+	syscall
 
-# Kör Multiplikation
-#li $t9, 1	
-# li $v0, 5
-# syscall
-# la $s0, ($v0)
-# li $v0, 5
-# syscall
-# la $s1, ($v0)
-# jal multiply
-# li $v0, 1
-# la $a0, ($s3)
-# syscall
-
-# Kör Fakultet
-# li $v0, 5
-# syscall
-# la $s2, ($v0)
-# jal faculty
-# li $v0, 1
-# la $a0, ($s3)
-# syscall
-# j end
-
+	li $v0, 5
+	syscall
+	beq $v0, 1, multiply
+	nop
+	beq $v0, 0, factorial
+	nop
 	
-#Multiplikationsfunktion
-multiply: #$s0 och $s1 är argumenten. Returnerar $s3
-li $t0, 0
-li $t1, 0
-loop1:
-add $t0, $s0, $t0
-addi $t1, $t1, 1
-bne $t1, $s1, loop1
+	li $v0, 4
+	la $a0, inv_input
+	syscall
 
-la $s3, ($t0) 
-beqz $t9, loop2 #för fakultetsfunktionen
+exit:
+	li $v0, 10
+	syscall
+		
+multiply:
+	li $v0, 4
+	la $a0, multi_msg
+	syscall
+	
+	li $v0, 5  #Load first integer
+	syscall
+	la $a0, ($v0)
+	
+	li $v0, 5 #Load second integer
+	syscall
+	la $a1, ($v0)
+	
+	jal multiply_func
+	
+	la $a0, ($v0)
+	li $v0, 1
+	syscall
+	
+	j exit
+	
+factorial:
+	li $v0, 4
+	la $a0, fact_msg
+	syscall
+	
+	li $v0, 5
+	syscall
+	la $a0, ($v0)
+	
+	jal factorial_func
+	
+	la $a0, ($v0)
+	li $v0, 1
+	syscall
+	
+	j exit
+	
+multiply_func:
+	li $t0, 0
+	la $t1, ($a0)
+	la $t2, ($a1)
+	
+	loop1:
+		add $t0, $t1, $t0
+		addi $t2, $t2, -1
+		bgtz $t2, loop1
+		
+	la $v0, ($t0)
+	jr $ra
 
-jr $ra
-
-#Fakultetsfunktion
-faculty: #$s2 är argumentet. Returnerar $s3
-li $t9, 0
-li $s3, 1
-loop2:
-la $s0, ($s3)
-la $s1, ($s2)
-addi $s2, $s2, -1
-bgtz $s2, multiply
-
-jr $ra
-
-#Stänger av programmet
-end:
-li $v0, 10
-syscall
-
-
+factorial_func:
+	la $t3, ($a0)
+	la $t4, ($ra) #Save return address
+	
+	loop2:
+	la $a0, ($v0) 
+	addi $t3, $t3, -1
+	la $a1, ($t3)
+	jal multiply_func
+	bgtz $t3, loop2
+	 
+	jr $t4
